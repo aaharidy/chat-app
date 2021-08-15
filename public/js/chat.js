fc2f48b049f9aecd67b288964c51753cf27feaf5
aaharidy <abdoharidy72@gmail.com>
2021-08-15 10:33:06 +0200

    location

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,11 +4,26 @@ socket.on('message', (message) => {
     console.log(message);
 })
 
-const form = document.querySelector('#chat')
+const form = document.querySelector('#chat'),
+    locationButton = document.querySelector('#sent-location')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const message = e.target.elements.message.value
 
     socket.emit('sendMessage', message)
+})
+
+locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Sorry geolocation not suppored from you browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords
+        socket.emit('sendLocation', {
+            latitude,
+            longitude
+        })
+    })
 })
